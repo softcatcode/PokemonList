2dbb06a1c5658fcb4245969223651e66c4fc0ed5
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-10 19:48:28 +0300

    Pokemon card is fixed.

--- app/src/main/java/com/softcat/vktest/presentation/pokemonList/pokemons.kt
+++ app/src/main/java/com/softcat/vktest/presentation/pokemonList/pokemons.kt
@@ -3,16 +3,17 @@ package com.softcat.vktest.presentation.pokemonList
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -21,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,10 +40,11 @@ fun PokemonCard(
     pokemon: Pokemon = Pokemon("spider", infoUrl = "https://pokemons/spider", iconUrl = "https://pokemons_icons/spider.png"),
     pokemonClickedListener: (Pokemon) -> Unit = {}
 ) {
+    val height = 90.dp
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp)
+            .height(height)
             .background(Color.White),
         onClick = { pokemonClickedListener(pokemon) }
     ) {
@@ -51,16 +54,20 @@ fun PokemonCard(
         ) {
             AsyncImage(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .clip(RoundedCornerShape(10.dp)),
+                    .size(height)
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.background),
                 model = pokemon.iconUrl,
-                contentDescription = null
+                contentDescription = "",
+                contentScale = ContentScale.Fit
             )
             Text(
+                modifier = Modifier.weight(1f),
                 text = pokemon.name,
                 textAlign = TextAlign.Center,
                 fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
+                fontSize = 30.sp
             )
         }
     }
