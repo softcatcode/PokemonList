e8c0df879d71be6b3f0a28c3ab242bb130556a0b
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-12 11:44:51 +0300

    Strings and colors were added.

--- app/src/main/java/com/softcat/vktest/ui/theme/Color.kt
+++ app/src/main/java/com/softcat/vktest/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightGrey = Color(0xFFC0BCBD)
+val TypeLabel = Color(0xFFF0B5C4)
+val PokemonIconCircle = Color(0xFFF5F0D3)
